2732e8db02bac62b1e5054dbeee1c5e2242646cb
Manrajjit Sidhu <33177027+manrajjitsidhu@users.noreply.github.com>
2023-12-06 09:59:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('For Jeff')
-df=pd.read_excel('/Data.xlsx',sheet_name="Import")
+dfPath='Data.xlsx'
+df=pd.read_excel(dfPath,sheet_name="Import")
 def get_image_link(image_filename):
     image_path = "/Plots/"+image_filename
     return f'<a href="{image_path}" target="_blank">Show Plot</a>'
