f508d0a217af0b36c9dea553d107ec58937ec91b
manrajjitsidhu <manrajjitsidhu@gmail.com>
2023-12-06 09:33:18 -0700

    test1

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('For Jeff')
+df=pd.read_excel('Data.xlsx',sheet_name="Import")
+def get_image_link(image_filename):
+    image_path = "/Plots/"+image_filename
+    return f'<a href="{image_path}" target="_blank">Show Plot</a>'
+
+df['Plot Link'] = df['Plot Name'].apply(get_image_link)
+st.markdown(df.to_markdown(index=False), unsafe_allow_html=True)
